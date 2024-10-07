a8a5713bc1a13407c353395d6ce9fcf8802d84b1
Rhysin <rhysin@rhysin.fyi>
2024-10-07 18:36:56 -0400

    FULLY Updated
    New parser checks for cleaner outputs in the title stripping and OMEGA index. Added a --omit to help clean up things

--- pdfparse.py
+++ pdfparse.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 
 pattern = r"(^[a-zA-Z0-9]{32}$)"
 wordspattern = r"^(?!.*[^\x20-\x7E])(?=[^\d:])[^\d:\n]{3,}(?: [^\d:\n]+)*"
-exclude = ["� SANS Institute","Edwinsberry","This page intentionally left blank.","Edwin ","Edwin Berry","Berry","ohNrhAfzA","live","lia","Licensed To","� SANS Institute  ","Index","� SANS Institute "]
+HARD_CODED_OMIT = ["� SANS Institute","this page intentionally left blank.","ohNrhAfzA","live","lia","Licensed To","� SANS Institute  ","Index","� SANS Institute "]
 characters_to_strip = "()'\":,”“‘?;-•’—…[]!"
 phrases_to_strip = ["'s", "'re", "'ve", "'t", "[0]", "[1]", "[2]", "[3]", "[4]", "[5]", "[6]"]
 delimeter = "Licensed To: "
@@ -27,13 +27,13 @@ def create_parser():
     parser = argparse.ArgumentParser(description="Command Line SANS PDF parser")
     parser.add_argument("-m", "--merge", action="store_true", help="Merge all PDFs into a single PDF (kinda useless but ¯\(ツ)/¯ )")
     parser.add_argument("-s", "--scrape", action="store_true", help="Scrape all title slides accross all PDFs")
-    parser.add_argument("--strip", metavar="Book5",type=str, help="This strips the book five index into search terms")
+    parser.add_argument("--index", metavar="Book5",type=str, help="This strips the book five index into search terms")
     parser.add_argument("--omega", action="store_true", help="Make an OMGEA index")
     parser.add_argument("--dcrypt", action="store_true", help="Decrypt all PDFs in directory ENCLOSE PASSWORD IN QUOTES")
     parser.add_argument("-p", "--passwd", metavar="PASS", type=str, help="Password to use with decryption WRAP PASSWORD IN QUOTES")
     parser.add_argument("-d", "--dir", metavar="Dir", type=str, help="Specify which directory SANS PDFs live in")
     parser.add_argument("-o", "--out", metavar="OutFile", type=str, help="Specify output file name")
-    parser.add_argument("-b", "--book", metavar="OutFile", type=str, help="Specify output file name")
+    parser.add_argument("--omit", metavar="NAME", type=str, nargs='*', help="Specify a list of strings to omit from the scrape")
     return parser
 
 def strip_characters(word):
@@ -70,7 +70,6 @@ def decrypt_pdfs(directory, outfile, passwd):
             
             pdf.save(f"{os.path.join(decrypt_path,outfile)}Book{counter}_Dcrypt.pdf")
         except pikepdf.PasswordError:
-            # Specific exception for password issues
             print(f"FAILED TO DECRYPT using pikepdf: {pdf_file}")
             print("TRYING WITH QPDF")
 
@@ -85,22 +84,6 @@ def decrypt_pdfs(directory, outfile, passwd):
 
     return
 
-def extract_slides_from_pdf(file_path,outputfile):
-
-    with pdfplumber.open(file_path) as pdf:
-        for pages in pdf.pages:
-            text = pages.extract_text()
-            page = pages.page_number
-
-            text = text.split('\n')
-            clean = text[1].replace(",", "")
-            if(re.search(pattern, clean)):
-                continue
-
-            else:
-                outputfile.write(f"{clean}, {page-2}\n")
-
-
 def pdf_merger(directory):
     files = os.listdir(directory)
 
@@ -112,18 +95,28 @@ def pdf_merger(directory):
             mergeFile.append(PyPDF2.PdfReader(pdf_name, 'rb'))
         
     mergeFile.write("SANSmerged.pdf")
-
-
+    
 def scrape_titles(directory,outputfile):
     files = os.listdir(directory)
-    output = open(outputfile, "w")
-
+    outputfile = open(outputfile, "w")
+    counter = 1
     for file in files:
         if(file.endswith("pdf")):
             pdf_name = os.path.join(directory,file)
-            extract_slides_from_pdf(pdf_name,output)
+            with pdfplumber.open(pdf_name) as pdf:
+                for pages in tqdm(pdf.pages, desc="Processing Title Slides"):
+                    text = pages.extract_text()
+                    page = pages.page_number
 
-    output.close()
+                    text = text.split('\n')
+                    clean = text[1].replace(",", "")
+                    if(re.search(pattern, clean)):
+                        continue
+                    else:
+                        outputfile.write(f"{clean}, BOOK: {counter} Page: {page-2}\n") 
+        counter += 1
+
+    outputfile.close()
 
 
 def check_if_good(word:str,lower_common):
@@ -138,7 +131,7 @@ def check_if_good(word:str,lower_common):
         return False
     if word.startswith("http://") or word.startswith("https://"):
         return False
-    if word in exclude:
+    if word in HARD_CODED_OMIT:
         return False
     if(re.match(pattern, word)):
         return False
@@ -147,31 +140,34 @@ def check_if_good(word:str,lower_common):
     return True
     
 
-def split_index(directory,book):
-    files = os.listdir(directory)
-    output = open("testomega.txt", "w")
-    fullIndex = []
-    book5 = os.path.join(directory,book)
-    with pdfplumber.open(book5) as pdf:
-        totalpages = pdf.pages
-        indexpage = input("Enter last page number before index starts: ")
-        indexpage = int(indexpage) + 2
-        for index in totalpages[indexpage:]:
-            mal = index.extract_text()
-            mal = mal.split('\n')
-            for words in mal:
-                if(re.search(wordspattern,words)):
-                    words = re.findall(wordspattern,words,re.MULTILINE)
-                    for text in words:
-                        stripped_text = text.lower().strip()
-                        # Check if the found text is in the exclude list
-                        if stripped_text not in (excl.lower() for excl in exclude):
-                            fullIndex.append(text)
-                            output.write(text + '\n') 
-                else:
-                    continue    
-                   
-    return fullIndex
+def split_index(directory, book, outfile, omit):
+    with open(outfile, "w") as output:
+        full_index = []
+        book_path = os.path.join(directory, book)
+        with pdfplumber.open(book_path) as pdf:
+            total_pages = pdf.pages
+            index_page = input("Enter the last page number before the index starts: ")
+            index_page = int(index_page) + 2
+
+            normalized_omit = [omit_word.lower() for omit_word in omit]
+            normalized_hardcoded_omit = [omit_word.lower() for omit_word in HARD_CODED_OMIT]
+
+            combined_omit = normalized_omit + normalized_hardcoded_omit
+
+            for index in total_pages[index_page:]:
+                mal = index.extract_text()
+                if mal: 
+                    mal_lines = mal.split('\n')
+                    for words in mal_lines:
+                        if re.search(wordspattern, words):
+                            found_words = re.findall(wordspattern, words, re.MULTILINE)
+                            for text in found_words:
+                                stripped_text = text.lower().strip()
+                                if not any(omit_phrase in stripped_text for omit_phrase in combined_omit):
+                                    full_index.append(stripped_text)
+                                    output.write(stripped_text + '\n')
+
+    return full_index
 
 def strip_all_pdfs(directory, outfile, giant_exclude):
     files = os.listdir(directory)
@@ -244,11 +240,12 @@ def main():
     wordlist = get_wordlist()
     parser = create_parser()
     args = parser.parse_args()
+    omitted_strings = args.omit if args.omit else []
 
     if args.dir and args.out:
         directory = args.dir
         outfile = args.out
-        book = args.strip
+        book = args.index
         if args.merge:
             pdf_merger(directory, outfile)
             return
@@ -258,8 +255,8 @@ def main():
         elif args.omega:
             omega_index(directory,outfile)
             return
-        elif args.strip:
-            split_index(directory,book)
+        elif args.index:
+            split_index(directory,book,outfile,omitted_strings)
         elif args.dcrypt:
             if args.passwd:
                 password = args.passwd
